fa8be9758d6abad7321b32d2704f5696b714c20a
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-11-29 19:53:06 +0200

    Add files via upload

--- task4/index.js
+++ task4/index.js
@@ -1,18 +1,23 @@
 /* eslint-disable no-unused-expressions */
 export const getTitle = () => {
   const elemTitle = document.querySelector(".title").textContent;
+  console.log(elemTitle);
+  return elemTitle;
 };
 export const getDescription = () => {
-  const aboutElem = document.querySelector(".about");
-  aboutElem.innerText;
+  const aboutElem = document.querySelector(".about").innerText;
+  console.log(aboutElem);
+  return aboutElem;
 };
 export const getPlans = () => {
-  const plansElem = document.querySelector(".plans");
-  plansElem.innerHTML;
+  const plansElem = document.querySelector(".plans").innerHTML;
+  console.log(plansElem);
+  return plansElem;
 };
 export const getGoal = () => {
-  const goalsElem = document.querySelector(".goal");
-  goalsElem.outerHTML;
+  const goalsElem = document.querySelector(".goal").outerHTML;
+  console.log(goalsElem);
+  return goalsElem;
 };
 getTitle();
 getDescription();
