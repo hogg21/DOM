4f198e5d35c4f9af391ed434f00d6993b50e2e63
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-11-30 17:03:37 +0200

    Add files via upload

--- task1(lesson22)/index.js
+++ task1(lesson22)/index.js
@@ -1,6 +1,11 @@
 const elemDiv = document.querySelector(".rect_div");
 const elemP = document.querySelector(".rect_p");
 const elemSpan = document.querySelector(".rect_span");
+const eventsListElem = document.querySelector(".events-list");
+const removeBtn = document.querySelector(".remove-handlers-btn");
+const clearButton = document.querySelector(".clear-btn");
+const addBtn = document.querySelector(".attach-handlers-btn");
+
 const logTarget = (text, color) => {
   const eventsListElem = document.querySelector(".events-list");
   eventsListElem.innerHTML += `<span style="color: ${color}; margin-left:8px;">${text}</span>`;
@@ -14,9 +19,30 @@ const logGreySpan = logTarget.bind(null, "SPAN", "grey");
 const logGreyP = logTarget.bind(null, "P", "grey");
 const logGreyDiv = logTarget.bind(null, "DIV", "grey");
 
-elemSpan.addEventListener("click", logGreySpan, true);
-elemSpan.addEventListener("click", logGreenSpan);
-elemP.addEventListener("click", logGreyP, true);
-elemP.addEventListener("click", logGreenP);
-elemDiv.addEventListener("click", logGreyDiv, true);
-elemDiv.addEventListener("click", logGreenDiv);
+const attachBtn = () => {
+  elemSpan.addEventListener("click", logGreySpan, true);
+  elemSpan.addEventListener("click", logGreenSpan);
+  elemP.addEventListener("click", logGreyP, true);
+  elemP.addEventListener("click", logGreenP);
+  elemDiv.addEventListener("click", logGreyDiv, true);
+  elemDiv.addEventListener("click", logGreenDiv);
+};
+const clearList = () => {
+  eventsListElem.innerHTML = "";
+};
+const removeBtnHandlers = () => {
+  elemSpan.removeEventListener("click", logGreySpan, true);
+  elemSpan.removeEventListener("click", logGreenSpan);
+
+  elemP.removeEventListener("click", logGreyP, true);
+  elemP.removeEventListener("click", logGreenP);
+
+  elemDiv.removeEventListener("click", logGreyDiv, true);
+  elemDiv.removeEventListener("click", logGreenDiv);
+};
+removeBtn.addEventListener("click", removeBtnHandlers);
+clearButton.addEventListener("click", clearList);
+addBtn.addEventListener("click", attachBtn);
+document.addEventListener("DOMContentLoaded", () => {
+  attachBtn();
+});
