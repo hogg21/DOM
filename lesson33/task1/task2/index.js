fbc5b129ce73e5c40025daf5ea77fcd3d4717ebe
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-12-12 00:44:12 +0200

    Add files via upload

--- lesson33/task1/task2/index.js
+++ lesson33/task1/task2/index.js
@@ -1,11 +1,15 @@
 const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
 
-function getTasksList() {
-  return fetch(baseUrl).then((response) => response.json());
+async function getTasksList() {
+  const response = await fetch(baseUrl).then((response) => response.json());
+  return response;
 }
 
-function getTaskById(taskId) {
-  return fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
+async function getTaskById(taskId) {
+  const response = await fetch(`${baseUrl}/${taskId}`).then((response) =>
+    response.json()
+  );
+  return response;
 }
 
 // examples
